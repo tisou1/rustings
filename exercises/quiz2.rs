9edf986ad7fae0004785ef5987b98ade01893cfc
tisou1 <57515396+tisou1@users.noreply.github.com>
2023-12-19 19:22:13 +0800

    chore: quiz2 done

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -20,7 +20,6 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
 
 pub enum Command {
     Uppercase,
@@ -32,11 +31,32 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
+        // input.iter() 迭代器时，每个元素都是 &(String, Command) 类型的引用
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            // 使用指令来处理command
+            // output.push()
+            let value = match command {
+                Command::Uppercase =>  string.to_uppercase(),
+                Command::Trim => string.trim().to_string(),
+                Command::Append(n) => {
+                    // for
+                    let mut appended = String::new();
+                    for _ in 0..*n {
+                        appended.push_str("bar");
+                    }
+                    string.to_string() + &appended
+
+                    // map + 闭包函数
+                    // let appended = (0..*n).map(|_| "bar").collect::<String>();
+                    // string.to_string() + &appended
+                }
+            };
+
+            output.push(value)
         }
         output
     }
@@ -45,7 +65,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use crate::my_module::transformer;
     use super::Command;
 
     #[test]
