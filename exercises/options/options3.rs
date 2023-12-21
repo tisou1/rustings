a35d3b1f0be0e81036e9cb6162a1b889e4f30d0a
tisou1 <57515396+tisou1@users.noreply.github.com>
2023-12-21 12:45:49 +0800

    chore: options done

--- exercises/options/options3.rs
+++ exercises/options/options3.rs
@@ -3,7 +3,6 @@
 // Execute `rustlings hint options3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 struct Point {
     x: i32,
@@ -14,7 +13,8 @@ fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
     match y {
-        Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+        // ref 关键字用于创建一个引用而不是移动 p 的所有权。这允许你在 match 表达式中访问 Point 结构体的字段而不移动它的所有权。
+        Some(ref p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => panic!("no match!"),
     }
     y; // Fix without deleting this line.
